a56d0613676b61739c4d44da41666af81d4a557d
Benja-san <beugnet.freelance@gmail.com>
2024-04-23 19:34:07 +0200

    Add data Add js Add dynamic content

---
+++ data/data.js
@@ -0,0 +1,59 @@
+const CHARACTERS = [
+  {
+    name: "Michael Scott",
+    position: "Regional Manager",
+    description:
+      "Michael Scott is the Regional Manager of the Scranton branch of Dunder Mifflin. He is a very childish and immature boss, but he is also very caring and loyal to his employees.",
+    picture: "mscott.png",
+  },
+  {
+    name: "Jim Halpert",
+    position: "Salesman",
+    description:
+      "Jim Halpert is a salesman at Dunder Mifflin. He is known for his pranks on Dwight Schrute, his coworker and rival.",
+    picture: "jhalpert.webp",
+  },
+  {
+    name: "Pam Beesly",
+    position: "Receptionist",
+    description:
+      "Pam Beesly is the receptionist at Dunder Mifflin. She is known for her artistic talent and her romantic relationship with Jim Halpert.",
+    picture: "pbeesly.png",
+  },
+  {
+    name: "Dwight Schrute",
+    position: "Salesman",
+    description:
+      "Dwight Schrute is a salesman at Dunder Mifflin. He is known for his eccentric personality and his rivalry with Jim Halpert.",
+    picture: "dshrute.png",
+  },
+  {
+    name: "Angela Martin",
+    position: "Accountant",
+    description:
+      "Angela Martin is an accountant at Dunder Mifflin. She is known for her strict adherence to rules and her secret relationship with Dwight Schrute.",
+    picture: "amartin.png",
+  },
+  {
+    name: "Ryan Howard",
+    position: "Temp",
+    description:
+      "Ryan Howard is a temp at Dunder Mifflin. He is known for his ambition and his complicated relationship with Kelly Kapoor.",
+    picture: "rhoward.png",
+  },
+  {
+    name: "Kelly Kapoor",
+    position: "Customer Service Rep",
+    description:
+      "Kelly Kapoor is a customer service rep at Dunder Mifflin. She is known for her obsession with celebrity culture and her complicated relationship with Ryan Howard.",
+    picture: "kkapoor.png",
+  },
+]
+
+const logoPicture = "office-logo.png"
+
+const globalColor = "transparent"
+
+const backgroundVideoContent = "U71w5tbq_PM"
+
+const mainTitle = "The Office"
